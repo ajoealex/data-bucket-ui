dfba4fb2a63ce892485f55c0d136c1813992f317
Ajoe Alex <ajoealex316@gmail.com>
2025-12-24 08:21:10 +0530

    update

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // Environment configuration for Data Bucket UI
 export const config = {
   // Community server URL - if empty, only show regular login
-  // communityServerUrl:  'https://databucket.automationhub.in',
-  communityServerUrl:  'http://127.0.0.1:8080',
+  communityServerUrl:  'https://databucket.automationhub.in',
+  // communityServerUrl:  'http://127.0.0.1:8080',
 
   // Maximum number of buckets per user/browser
   maxBucketsPerUser: 5,
