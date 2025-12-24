5c7cd3fec3fbb34d61ca1a6a32ef5e05305d0592
Ajoe Alex <ajoealex316@gmail.com>
2025-12-24 08:17:39 +0530

    test

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
 export const config = {
   // Community server URL - if empty, only show regular login
   communityServerUrl:  'https://databucket.automationhub.in',
+  communityServerUrl:  'http://127.0.0.1:8080',
 
   // Maximum number of buckets per user/browser
   maxBucketsPerUser: 5,
